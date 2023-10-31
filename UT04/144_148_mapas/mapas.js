1eba3c95d3cd213b34ee93388d7a3c2ca3579656
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-10-31 10:44:16 +0100

    cosas

--- UT04/144_148_mapas/mapas.js
+++ UT04/144_148_mapas/mapas.js
@@ -15,7 +15,10 @@ function asignarValoresMapasEnUnaFila() {
 
 function usoArrayParaCrearMapa() {
     const personas = new Map([[1, "Jose"], [2, "Maria"], [3, "Elena"]]);
+    const arrayPersonas = [[1, "Jose"], [2, "María"], [3, "Elena"]];
+    const personasParametroArray = new Map(arrayPersonas);
     console.log(personas);
+    console.log(personasParametroArray);
 }
 
 function obtenerValoresMapa() {
@@ -50,6 +53,59 @@ function borrarElementoMapa() {
     console.log(provincias);
 }
 
+function obtenerClavesYValoresForOf() {
+    const provincias = new Map();
+    provincias.set(1,"Álava");
+    provincias.set(28,"Madrid");
+    provincias.set(34,"Palencia");
+    provincias.set(41,"Sevilla");
+    let claves = provincias.keys();
+    for (let k of claves) {
+        console.log(k);
+    }
+    let valores = provincias.values();
+    for (let v of valores) {
+        console.log(v);
+    }
+}
+
+function convertirMapaArray() {
+    const provincias = new Map();
+    provincias.set(1,"Álava").set(28,"Madrid").set(34,"Palencia").set(41,"Sevilla");
+    console.log(...provincias);
+}
+
+function recorrerMapaForOf() {
+    const provincias = new Map();
+    provincias.set(1,"Álava").set(28,"Madrid").set(34,"Palencia").set(41,"Sevilla");
+    for (let elemento of provincias) {
+        console.log(elemento);
+    }
+}
+
+function recuperarClaveValorEnMismoBucleForOf() {
+    const provincias = new Map();
+    provincias.set(1,"Álava").set(28,"Madrid").set(34,"Palencia").set(41,"Sevilla");
+    for (let [clave, valor] of provincias) {
+        console.log(`Clave: ${clave}, valor: ${valor}`);
+    }
+}
+
+function obtenerClavesYValoresForOfTodoEnBucle() {
+    const provincias = new Map();
+    provincias.set(1,"Álava");
+    provincias.set(28,"Madrid");
+    provincias.set(34,"Palencia");
+    provincias.set(41,"Sevilla");
+    for (let k of provincias.keys()) {
+        console.log(k);
+    }
+    for (let v of provincias.values()) {
+        console.log(v);
+    }
+
+}
+
 window.onload = function() {
     document.getElementById("asignarValoresMapas").addEventListener("click", asignarValoresMapas);
     document.getElementById("asignarValoresMapasEnUnaFila").addEventListener("click", asignarValoresMapasEnUnaFila);
@@ -57,4 +113,8 @@ window.onload = function() {
     document.getElementById("obtenerValoresMapa").addEventListener("click", obtenerValoresMapa);
     document.getElementById("buscarClaveMapa").addEventListener("click", buscarClaveMapa);
     document.getElementById("borrarElementoMapa").addEventListener("click", borrarElementoMapa);
+    document.getElementById("obtenerClavesYValoresForOf").addEventListener("click", obtenerClavesYValoresForOf);
+    document.getElementById("convertirMapaArray").addEventListener("click", convertirMapaArray);
+    document.getElementById("recorrerMapaForOf").addEventListener("click", recorrerMapaForOf);
+    document.getElementById("obtenerClavesYValoresForOfTodoEnBucle").addEventListener("click", obtenerClavesYValoresForOfTodoEnBucle);
 }
