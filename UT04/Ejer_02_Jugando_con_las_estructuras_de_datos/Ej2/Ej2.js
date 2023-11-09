f9d2ff4d0f5df6ed10cdf531bc8e14b09c1629a1
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-11-09 16:38:25 +0100

    cositas en el bingo de mi aguela

--- UT04/Ejer_02_Jugando_con_las_estructuras_de_datos/Ej2/Ej2.js
+++ UT04/Ejer_02_Jugando_con_las_estructuras_de_datos/Ej2/Ej2.js
@@ -1,45 +1,63 @@
-let matrizNumeros = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-    [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-    [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-    [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
-    [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
-    [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-    [80, 81, 82, 83, 84, 85, 86, 87, 88, 89]
-];
-
-function escogerNumeros(matrizNumeros) {
-    let contador = 0;
-    let num = [3][8];
-    do {
-        for (let i = 0; i < matrizNumeros.length; i++) {
-            for (let j = 0; j < matrizNumeros[i].length; j++) {
-                let r = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-                num.push(matrizNumeros[i][r]);
-            }
-        }
-    } while (contador < 4);
-    num.sort();
-    return num;
+function shuffle(array) {
+    return array.sort(function(a, b) {
+      return Math.random() - 0.5;
+    });
 }
 
-function borrarNumeros(num) {
-    let rHuecos = Math.floor(Math.random() * (2 - 1 + 1) + 1);
-    let contador = 0;
-    do {
-        for (let i = 0; i < num.length; i++) {
-            
-        }
-    } while (contador < rHuecos);
-    return num;
+function generarColumnas(n) {
+    let numeros = [0,1,2,3,4,5,6,7,8,9];
+    let columna = [];
+
+    shuffle(numeros);
+
+    for (let i = 0; i < 3; i++){
+        columna[i] = numeros[i] + (n * 10);
+    }
+    columna.sort();
+    console.log(columna);
+    return columna;
 }
 
-function pintar() {
+function eliminarNumeros(columna, posicionHuecos) {
+    for (let i = 0; i < columna.length; i++) {
+        columna[posicionHuecos[i]] = 0;
+    }
+    return columna;
+}
+
+function posicionHuecos() {
+    let cantidadBorrados = Math.floor(Math.random() * 2) + 1;
+    let contador = 0, posiconActual;
+    const arrayPosiciones = [];
     
+    while (contador < cantidadBorrados) {
+
+        posiconActual = Math.floor(Math.random() * 3);
+
+        if (!arrayPosiciones.includes(posiconActual)) {
+            arrayPosiciones.push(posiconActual);
+            contador++;
+        }
+    }
+    return arrayPosiciones;
 }
+
+function pintarCarton() {
+    let listaNumeros = [];
+    for (let i = 0; i < 9; i++) {
+        listaNumeros = eliminarNumeros(generarColumnas(i), posicionHuecos());
+        for (let j = 0; j < 3; j++) {
+            if (listaNumeros[j] == 0) {
+                document.getElementById(`celda${i}${j}`).textContent = "";
+                document.getElementById(`celda${i}${j}`).classList.remove("circulos");
+            } else {
+                document.getElementById(`celda${i}${j}`).textContent = listaNumeros[j];
+            }       
+        }
+    }
+}
+
 
 window.onload = function() {
-    pintar();
+    pintarCarton();
 }
