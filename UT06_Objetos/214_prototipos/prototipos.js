85ae4b39ebf944dccd60764806451c3c0109a827
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-11-20 12:55:07 +0100

    xddddd

--- UT06_Objetos/214_prototipos/prototipos.js
+++ UT06_Objetos/214_prototipos/prototipos.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-    
+    document.getElementById("prototipo").addEventListener("click",prototipo);
+    document.getElementById("aniadirPrototipo").addEventListener("click",aniadirPrototipo);
+    document.getElementById("mostrarDosObjetos").addEventListener("click",mostrarDosObjetos);
+    document.getElementById("modificarPropiedadPrototipo").addEventListener("click",modificarPropiedadPrototipo);
 }
