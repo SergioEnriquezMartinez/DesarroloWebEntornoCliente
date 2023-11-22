2bcfafed3701d5377c13d19f1294188c6e3082d4
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-11-22 14:28:50 +0100

    cosas que mas o menos funcionan

--- UT05_Funciones/Ejer_03_Funciones/Ej3/Ej3.js
+++ UT05_Funciones/Ejer_03_Funciones/Ej3/Ej3.js
@@ -1,3 +1,4 @@
+/*---------------------APARTADO 3.1-----------------------------*/ 
 let x = "Esto es una variable global";
 
 function funcionVariable() {
@@ -11,6 +12,38 @@ function variableBucle() {
         document.getElementById("rBucle").textContent += x + " " + i + "\n";
     }
 }
+
+/*---------------------APARTADO 3.2-----------------------------*/
+function crearArray() {
+    let array = new Array(10);
+    return array;
+}
+
+function rellenarArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 100);
+    }
+    return array;
+}
+
+function ordenarArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        let aux = array[i];
+        let j = i - 1;
+
+        while (j >= 0 && array[j] > aux) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = aux;
+    }
+    return array;
+}
+
+function desordenarArray(array) {
+    
+}
+
 window.onload = function() {
     document.getElementById("vGlobal").addEventListener("click",() => {document.getElementById("rGlobal").textContent = x});
     document.getElementById("vFuncion").addEventListener("click",funcionVariable);
