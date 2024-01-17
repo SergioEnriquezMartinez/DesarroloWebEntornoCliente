1478d99c429c038c715f8e0d91140c6764c113fb
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2024-01-17 12:54:50 +0100

    hemos avanzado la hsotia

--- Diana_dardos/dianaDardos.js
+++ Diana_dardos/dianaDardos.js
@@ -1,18 +1,25 @@
+let contadorClicks = 0;
+
+function posicionarDardo(color, x, y) {
+    let imagen = new Image();
+    imagen.src = "img/" + color + ".png";
+
+    imagen.style.height = "10%";
+    imagen.style.position = "absolute";
+    imagen.style.left = x + "px";
+    imagen.style.top = y + "px";
+
+    document.body.appendChild(imagen);
+}
+
 window.onload = function () {
-    document.getElementById("diana").addEventListener("click", function (evento) {
-        let imagen = new Image();
-        imagen.src = "img/azul.png";
-        let x = evento.clientX;
-        let y = evento.clientY;
-
-        imagen.style.width = "10%";
-        imagen.style.height = "10%";
-
-        imagen.style.position = "absolute";
-        imagen.style.left = x + "px";
-        imagen.style.top = y + "px";
-        
-        document.body.appendChild(imagen);
-    }); 
-
+    document.getElementById("circulo1").addEventListener("click", (evento) => {
+        if (contadorClicks < 6) {
+            let color = contadorClicks % 2 === 0 ? "azul" : "amarillo";
+            
+            posicionarDardo(color, evento.clientX, evento.clientY);
+            
+            contadorClicks++;
+        }
+    });
 }
