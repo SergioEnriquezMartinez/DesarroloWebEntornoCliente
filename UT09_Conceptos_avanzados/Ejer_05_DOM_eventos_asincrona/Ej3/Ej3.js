a13027912e6aebde8ce6790477d14a909de6350f
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2024-01-25 16:58:48 +0100

    ejercicio 3 terminado

--- UT09_Conceptos_avanzados/Ejer_05_DOM_eventos_asincrona/Ej3/Ej3.js
+++ UT09_Conceptos_avanzados/Ejer_05_DOM_eventos_asincrona/Ej3/Ej3.js
@@ -3,6 +3,7 @@ function recogerClassName () {
     let arrayElementosClase = Array.from(elementosClase);
 
     let listaClase = document.getElementById("listaClase");
+    let botonesClase = document.getElementById("botonesClase");
     
     arrayElementosClase.forEach(elemento => {
         let fila = document.createElement("li");
@@ -10,13 +11,37 @@ function recogerClassName () {
         listaClase.appendChild(fila);
     });
 
-    let boton = document.createElement("button");
-    boton.textContent = "Eliminar";
-    boton.addEventListener("click", () => {
+    let botonManual = document.createElement("button");
+    botonManual.textContent = "Eliminar Manualmente";
+    botonManual.addEventListener("click", () => {
         listaClase.removeChild(listaClase.children[0]);
     });
 
-    listaClase.appendChild(boton);
+    botonesClase.appendChild(botonManual);
+
+    let intervalId;
+    let botonAutomatico = document.createElement("button");
+    botonAutomatico.textContent = "Eliminar Automaticamente";
+
+    botonAutomatico.addEventListener("click", () => {
+        if (listaClase.children.length > 0) {
+            if (!intervalId) {
+                intervalId = setInterval(() => {
+                    listaClase.removeChild(listaClase.children[0]);
+
+                    if (listaClase.children.length === 0) {
+                        clearInterval(intervalId);
+                        intervalId = null;
+                    }
+                }, 1000);
+            } else {
+                clearInterval(intervalId);
+                intervalId = null;
+            }
+        }
+    });
+
+    botonesClase.appendChild(botonAutomatico);
 }
 
 function recogerSelector () {
@@ -24,20 +49,46 @@ function recogerSelector () {
     let arrayElementosSelector = Array.from(elementosSelector);
 
     let listaSelector = document.getElementById("listaSelector");
+    let botonesSelector = document.getElementById("botonesSelector");
+
 
     arrayElementosSelector.forEach(elemento => {
         let fila = document.createElement("li");
         fila.textContent = elemento.textContent + " recogido con querySelectorAll";
         listaSelector.appendChild(fila);
     });
-    let boton = document.createElement("button");
-    boton.textContent = "Eliminar";
+    let botonManual = document.createElement("button");
+    botonManual.textContent = "Eliminar Manualmente";
 
-    boton.addEventListener("click", () => {
+    botonManual.addEventListener("click", () => {
         listaSelector.removeChild(listaSelector.children[0]);
     });
 
-    listaSelector.appendChild(boton);
+    botonesSelector.appendChild(botonManual);
+    
+    let intervalId;
+    let botonAutomatico = document.createElement("button");
+    botonAutomatico.textContent = "Eliminar Automaticamente"
+
+    botonAutomatico.addEventListener("click", () => {
+        if (listaSelector.children.length > 0) {
+            if (!intervalId) {
+                intervalId = setInterval(() => {
+                    listaSelector.removeChild(listaSelector.children[0]);
+
+                    if (listaSelector.children.length === 0) {
+                        clearInterval(intervalId);
+                        intervalId = null;
+                    }
+                }, 1000);
+            } else {
+                clearInterval(intervalId);
+                intervalId = null;
+            }
+        }
+    });
+
+    botonesSelector.appendChild(botonAutomatico);
 }
 
 
